9129b9b4242f3a6b33ffcbae766e8181320e4a97
orhungkdmr <orhungokdemir@gmail.com>
2025-11-21 08:49:43 +0300

    Build debug ls

--- build.sh
+++ build.sh
@@ -2,13 +2,12 @@
 # exit on error
 set -o errexit
 
-echo "--- 1. PAKETLER YUKLENIYOR ---"
+echo "---------------------------------------------"
+echo "--- DEDEKTIF BASLADI: static ICINDE NE VAR? ---"
+ls -R static/
+echo "---------------------------------------------"
+
+# Normal kurulum devam ediyor
 pip install -r requirements.txt
-
-echo "--- 2. STATIK DOSYALAR TOPLANIYOR (DETAYLI MOD) ---"
-# --clear: Eski dosyaları siler
-# --verbosity 2: İşlenen her dosyayı ekrana yazar
 python manage.py collectstatic --no-input --clear --verbosity 2
-
-echo "--- 3. VERITABANI GUNCELLEMESI ---"
 python manage.py migrate
